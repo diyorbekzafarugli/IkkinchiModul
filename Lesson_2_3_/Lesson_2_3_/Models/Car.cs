c325924550119137aa5e559e27cae2affdc898c6
Diyorbek Boriboyev <diyorbekzafarugli@gmail.com>
2026-01-27 12:25:42 +0500

    done

---
+++ Lesson_2_3_/Lesson_2_3_/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson_2_3_.Models;
+
+public class Car
+{
+    public Guid CarId { get; set; }
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public int Speed { get; set; }
+
+
+    public Car()
+    {
+        SayHello();
+    }
+    public Car(string model, string brand, string color, int speed)
+    {
+        Model = model;
+        Brand = brand;
+        Color = color;
+        Speed = speed;
+    }
+    public void SayHello()
+    {
+        Console.WriteLine("Assalomu Alaykum");
+
+        Car car = new Car("Nexia", "GM", "Black", 180);
+        Console.WriteLine(car.Model);
+        Console.WriteLine(car.Color);
+        Console.WriteLine(car.Speed);
+        Console.WriteLine(car.Brand);
+    }
+}
---
+++ Lesson_2_3_/Lesson_2_3_/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lesson_2_3_.Models;
+
+namespace Lesson_2_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Car Cars = new Car();
+            Console.WriteLine("---------------------");
+            Car Cars1 = new Car("X5", "BMW", "Black", 300);
+            Console.WriteLine(Cars1.Model);
+            Console.WriteLine(Cars1.Brand);
+            Console.WriteLine(Cars1.Color);
+            Console.WriteLine(Cars1.Speed);
+        }
+    }
+}
